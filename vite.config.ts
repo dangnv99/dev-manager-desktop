0236f8de9ec53370dd7a411ff9f2279bfd837eaa
dangnv99 <dangphucnguyen0309@gmail.com>
2025-08-07 23:21:46 +0700

    dangnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dev-manager-desktop/", // <--- Đặt đúng tên repo ở đây!
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
